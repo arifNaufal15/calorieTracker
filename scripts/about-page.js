8843f76f8422a3a92c3c9bf0486bef7650d3ec43
arifNaufal15 <arifnaufal219@gmail.com>
2025-06-01 11:18:16 +0800

    change json path

--- scripts/about-page.js
+++ scripts/about-page.js
@@ -1,4 +1,51 @@
+/*
 const infoCards = document.querySelectorAll('.info');
+const overlay = document.getElementById('popup-overlay');
+const popupContent = document.getElementById('popup-content');
+const popupClose = document.getElementById('popup-close');
+
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        const peopleData = data;
+
+        infoCards.forEach(card => {
+            const id = card.getAttribute('data-id');
+            const person = peopleData[id];
+
+            if (person) {
+                    card.innerHTML =
+                        `<img src="${person.img}" alt="${person.name}" style="width:100px; height:100px; border-radius:12px; box-shadow:0 2px 6px rgba(0,0,0,0.2);"><br><br>
+                        <p>${person.name}</p>`;
+            }
+
+            card.addEventListener('click', () => {
+                if (person) {
+                    popupContent.innerHTML =
+                        `<img src="${person.img}" alt="${person.name}" style="width:100px; height:100px; border-radius:12px; box-shadow:0 2px 6px rgba(0,0,0,0.2);"><br><br>
+                        <h3>${person.name}</h3>
+                        <p>Matric No:</strong> ${person.matric}</p>
+                        <p>Email:</strong> ${person.email}</p>
+                        <p>${person.bio}</p>`;
+
+                    overlay.classList.remove('hidden');
+                }
+            });
+        });
+    })
+
+popupClose.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+        overlay.classList.add('hidden');
+    }
+});
+
+*/
+const personalInfoContainer = document.querySelector('.personal-info-container');
 const overlay = document.getElementById('popup-overlay');
 const popupContent = document.getElementById('popup-content');
 const popupClose = document.getElementById('popup-close');
@@ -7,7 +54,12 @@ fetch("/data/team-members.json")
     .then(response => response.json())
     .then(data => {
         const peopleData = data;
+        for(let i = 1; i <= Object.keys(peopleData).length; i++)
+        {
+            personalInfoContainer.innerHTML += `<div class="info" data-id="${i}"></div>`
+        }
 
+        const infoCards = document.querySelectorAll('.info');
         infoCards.forEach(card => {
             const id = card.getAttribute('data-id');
             const person = peopleData[id];
